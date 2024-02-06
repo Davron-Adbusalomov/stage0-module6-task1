1f6f7b7cae9c4be21d8e01ebce30edc584ca39de
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-06 10:53:45 +0500

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,12 +1,12 @@
 package com.epam.mjc.stage0;
 
 public class Bird extends Animal{
-  public Bird(){
+  Bird(){
       super("blue", 2, false);
   }
 
   @Override
-  public String getDescription(){
+ String getDescription(){
     return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
   }
 }
